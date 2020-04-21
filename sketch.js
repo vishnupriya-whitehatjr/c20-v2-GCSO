6ac047c7a0f2c286de01d9c51776b1de948465d1
vishnupriya-whitehatjr <whitehatjr.priya@gmail.com>
2020-04-22 00:40:31 +0530

    commit

--- sketch.js
+++ sketch.js
@@ -3,14 +3,13 @@ var deformation;
 
 function setup() {
   createCanvas(1000, 400);
-
-  wall = createSprite(900,200,25,300);
-  wall.shapeColor ="lightpink";
   
   car = createSprite(100,190,50,50);
   car.shapeColor ="lightpink"
   car.velocityX= 10;
-
+  wall = createSprite(900,200,20,300);
+  wall.shapeColor ="lightpink";
+  
   speed=random(55,90);
   weight=random(400,1500);
   
@@ -25,22 +24,21 @@ function draw() {
   textSize(25);
   text("GLOBAL CAR SAFETY ORGANIZATION",250,50);
   
-  car.collide(wall,deformations);
+  //refer to function collide.
+  collide(wall,car)
   
   textSize(15);
-  
   text("Deformation is : > 180 ===> Car is Lavender ===> Lethal for the Passangers",30,340);
   text("Deformation is : < 180 && > 100 ===> Car is Yellow ===> Average for the Passangers",30,360);
   text("Deformation is : < 100 ===> Car is LightGreen ===> Good for the Passangers",30,380);
   fill("red");
-  textSize(20);
+  textSize(25);
   text("DEFORMATION IS : "+Math.floor(deformation),30,300);
 }
 
 function deformations(){
 
   deformation=(0.5 * weight * speed * speed)/22500;
-  //console.log(deformation);
   
 	if(deformation>180)
 	{
@@ -57,4 +55,13 @@ function deformations(){
 		car.shapeColor="lightgreen";
   }
   
+}
+function collide(object1,object2){
+  if(object1.x-object2.x===object1.width/2 + object1.width/2+10)
+    {
+      deformations();
+      object1.velocityX = 0;
+      object2.velocityX = 0;
+     
+    }
 }
